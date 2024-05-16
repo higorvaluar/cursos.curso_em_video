c1a8930a43df79df392e55ee0565539cb27d1ff3
higorvaluar <higorvb2001@gmail.com>
2024-05-16 04:24:14 -0300

    Módulos e Aulas de JS

---
+++ JavaScript - (40 Horas)/Módulo B/Aula 05/ex001.js
@@ -0,0 +1,9 @@
+// Aqui foram declaradas 3 variáveis (n1, n2, n3) que recebem seus respectivos números.
+var n1 = 5  
+var n2 = 8.5
+var n3 = 15
+
+// No JavaScript, podemos usar 3 tipos de aspas para declarar texto, são elas ("") Aspas duplas; ('') Aspas simples; (``) Crase.
+var s1 = "JavaScript"
+var s2 = 'Curso em Vídeo'
+var s3 = `Higor Valuar`
